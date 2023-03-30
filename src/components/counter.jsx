f1f640bfd8f52c856810ed0dc8c94e8a2fff55c7
khubaib-ali-codegenio <117726252+khubaib-ali-codegenio@users.noreply.github.com>
2023-03-30 12:07:01 +0500

    Binding this to functions with constructor

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,41 +2,44 @@ import React, {Component} from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0,
-        imageUrl: 'https://picsum.photos/200',
-        tags: []
+        count: 1,
+        imageUrl: 'https://picsum.photos/200'
     };
-
     styles = {
         fontSize: 10,
         fontWeight: 'bold',
     }
 
-    renderTags() {
-        if (this.state.tags.length === 0) return <p>There are no tags</p>
+    constructor() {
+        super();
+        this.handleIncrement = this.handleIncrement.bind(this)
+    }
 
-        return <ul>
-            {
-                this.state.tags.map(tag => <li key={tag}>{tag}</li>)
-            }
-        </ul>
+    handleIncrement() {
+        this.state.count += 1;
     }
 
     render() {
 
         return (
             <div>
-                {
-                    this.state.tags.length === 0 && <p>please create a new tag.</p>
-                }
-                {
-                    this.renderTags()
-                }
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
             </div>
         );
     }
 
 
+    getBadgeClasses() {
+        let classes = "badge m-2 bg-";
+        classes += this.state.count === 0 ? 'warning' : 'primary';
+        return classes;
+    }
+
+    formatCount() {
+        const {count} = this.state;
+        return count === 0 ? "Zero" : count;
+    }
 }
 
 export default Counter;
