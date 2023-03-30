3fd18af9201d42b893e2b394bfaba84ef4a18b87
khubaib-ali-codegenio <117726252+khubaib-ali-codegenio@users.noreply.github.com>
2023-03-30 11:25:33 +0500

    Add First Component and bootstrap

---
+++ src/dev/previews.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {ComponentPreview, Previews} from '@react-buddy/ide-toolbox'
+import {PaletteTree} from './palette'
+import App from "../App";
+import Counter from "../components/counter";
+
+const ComponentPreviews = () => {
+    return (
+        <Previews palette={<PaletteTree/>}>
+            <ComponentPreview path="/App">
+                <App/>
+            </ComponentPreview>
+            <ComponentPreview path="/Counter">
+                <Counter/>
+            </ComponentPreview>
+        </Previews>
+    )
+}
+
+export default ComponentPreviews
